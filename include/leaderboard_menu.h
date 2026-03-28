ad846f80d3179f77fa8186a4aa3577d679def089
zxbwr347 <girosacramento@gmail.com>
2026-03-28 19:16:56 +0800

    uploaded header file

---
+++ include/leaderboard_menu.h
@@ -0,0 +1,22 @@
+/*
+
+	leaderboard_menu.h
+	
+	Header file for leaderboard_menu.c
+	
+	Author: SACRAMENTO, Yvan Gregorio A.
+	Last Modified: 3-28-2026
+
+*/
+
+#ifndef LEADERBOARD_MENU
+#define LEADERBOARD_MENU
+
+#include "user_struct.h"
+#include "species_struct.h"
+
+void leaderboardMenu(UserData userData, SDB speciesDataBase);
+void UserLeaderboard(UserData database);
+void SpeciesLeaderboard(SDB database);
+
+#endif
