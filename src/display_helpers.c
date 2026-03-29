7b20ec5fcda86bd3d0ab28036def527669c788d6
K97i <105120272+K97i@users.noreply.github.com>
2026-03-29 17:41:24 +0800

    Add docu to display_helpers.c

--- src/display_helpers.c
+++ src/display_helpers.c
@@ -16,23 +16,56 @@
 #include "species_struct.h"
 #include "string_helpers.h"
 
+/*
+
+	@name	printSpecies();
+
+    @brief	Basic print of the specimen name
+
+    @param	species     Pointer of the species data
+    @param	numbered	Pointer of the index of the species to edit
+
+*/
 void printSpecies(Species *species, int numbered) {
+    // If number != 0, print the number
     if (numbered)
         printf("%d: ", numbered);
 
     printf("%s\n", species->name);
 }
 
+/*
+
+	@name	printSpecies();
+
+    @brief	Basic print of the specimen name, but for SpeciesData
+
+    @param	species     Pointer of the species data
+    @param	numbered	Pointer of the index of the species to edit
+
+*/
 void printSpeciesData(SpeciesData *species, int numbered) {
+    // If number != 0, print the number
     if (numbered)
         printf("%d: ", numbered);
 
     printf("%s\n", species->name);
 }
 
+/*
+
+	@name	printSpeciesDatabaseInfo();
+
+    @brief	Prints the metadata from the species database
+
+    @param	species     Pointer of the species data
+
+*/
 void printSpeciesDatabaseInfo(SpeciesData *species) {
     char conservationStatus[WORD_LIMIT] = "";
     
+    // Set proper conservation status in words
+    // 2 => Least Concern, 1 => Vulnerable, 0 => Extinct
     if (species->conservationStatus == 2)
         strcpy(conservationStatus, "Least Concern");
 
@@ -47,17 +80,30 @@ void printSpeciesDatabaseInfo(SpeciesData *species) {
     printf("Biome: %s\n", species->biome);
 }
 
+/*
+
+	@name	printSpeciesSpecialInfo();
+
+    @brief	Prints all available metadata for the specimen
+
+    @param	species     Pointer of the species data
+    @param	species     Pointer of the species array
+    @param	species     Index of the species in the species array 
+
+*/
 void printSpeciesSpecialInfo(Species *species, SDB *sDB, int databaseIndex) {
     // CALCULATION 1
     char sex[WORD_LIMIT] = "";
     float bodyMassIndex = species->weight * (species->height * species->height);
 
+    // Get the sex of the specimen
     if (species->sex == 0)
         strcpy(sex, "Undefined");
 
     else
         species->sex % 2 ? strcpy(sex, "Male") : strcpy(sex, "Female");
 
+    // If databaseIndex is set, print the metadata from the database 
     if (databaseIndex != -1)
         printSpeciesDatabaseInfo(&sDB->species[databaseIndex]);
 
